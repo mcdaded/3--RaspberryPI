2649c87c0a2fa525ad2518247b5fe3fe2319b486
mcdaded <daniel.r.mcdade@gmail.com>
2016-01-05 22:06:51 -0500

    updates

--- schedule_install.py
+++ schedule_install.py
@@ -4,11 +4,10 @@ Raspberry PI - 'schedule_install.py' created on 1/5/2016 at 2:04 PM
 @author: dmcdade
 """
 
-activate_this = '/Desktop/Projects/environment/venv/bin/activate_this.py'
+activate_this = '/home/pi/Desktop/Projects/environments/venv/bin/activate_this.py'
 execfile(activate_this, dict(__file__=activate_this))
 
 from subprocess import Popen, PIPE
 
 session = Popen(['pip', 'install numpy'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-session.stdin.write(sqlCommand)
 session.communicate()
