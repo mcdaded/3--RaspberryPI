67d4214e5e3d300a7a16746eade07b19ca27aa0b
danielmcdade <dmcdade@insightsquared.com>
2016-01-06 13:00:17 -0500

    testing update

--- schedule_install.py
+++ schedule_install.py
@@ -7,7 +7,7 @@ Raspberry PI - 'schedule_install.py' created on 1/5/2016 at 2:04 PM
 activate_this = '/home/pi/Desktop/Projects/environments/venv/bin/activate_this.py'
 execfile(activate_this, dict(__file__=activate_this))
 
-from subprocess import Popen, PIPE
+from subprocess import call
 
-session = Popen(['pip', 'install numpy'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-session.communicate()
+call(["pip", "install", "numpy"])
+# call(["pip", "install", "pandas", "--upgrade"])
